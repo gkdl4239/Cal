780438b30a658fdace34fcc62ddf17eff9b9b252
gkdl4239 <gkdl4239@gmail.com>
2024-11-15 15:16:49 +0900

    Refactor

--- src/Lv3Calculator/com/example/calculator2/App.java
+++ src/Lv3Calculator/com/example/calculator2/App.java
@@ -3,19 +3,25 @@ package Lv3Calculator.com.example.calculator2;
 import java.util.Scanner;
 
 public class App {
-    public App() {
-    }
-
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+
+        Calculator2<Integer, Integer> cal1;
+        Calculator2<Integer, Double> cal2;
+        Calculator2<Double, Integer> cal3;
+        Calculator2<Double, Double> cal4;
         Results results = new Results();
+        String delete;
+        String menu;
 
         while(true) {
             while(true) {
                 System.out.println("번호를 입력해주세요");
                 System.out.println("1.계산 2.조회 3.삭제 4.종료");
-                String menu = sc.next();
-                String delete;
+                menu = sc.next();
+
+
+                //계산
                 if (menu.equals("1")) {
                     System.out.print("첫 번째 숫자를 입력하세요:");
                     delete = sc.next();
@@ -29,44 +35,53 @@ public class App {
                     Number result = 0;
                     OperatorType op;
                     if (num1 instanceof Integer && num2 instanceof Integer) {
-                        Calculator2<Integer, Integer> cal1 = new Calculator2((Integer)num1, (Integer)num2);
+                        cal1 = new Calculator2<>((Integer)num1,(Integer)num2);
                         op = OperatorType.fromSymbol(operator);
                         result = cal1.calculate(op);
                     } else if (num1 instanceof Integer && num2 instanceof Double) {
-                        Calculator2<Integer, Double> cal2 = new Calculator2((Integer)num1, (Double)num2);
+                        cal2 = new Calculator2<>((Integer)num1,(Double)num2);
                         op = OperatorType.fromSymbol(operator);
                         result = cal2.calculate(op);
                     } else if (num1 instanceof Double && num2 instanceof Integer) {
-                        Calculator2<Double, Integer> cal3 = new Calculator2((Double)num1, (Integer)num2);
+                        cal3 = new Calculator2<>((Double)num1,(Integer)num2);
                         op = OperatorType.fromSymbol(operator);
                         result = cal3.calculate(op);
                     } else if (num1 instanceof Double && num2 instanceof Double) {
-                        Calculator2<Double, Double> cal4 = new Calculator2((Double)num1, (Double)num2);
+                        cal4 = new Calculator2<>((Double)num1,(Double)num2);
                         op = OperatorType.fromSymbol(operator);
                         result = cal4.calculate(op);
                     }
 
                     System.out.println("결과 : " + result);
                     System.out.println("결과 값이 저장 되었습니다.");
-                    results.setQueue((Number)result);
-                } else if (menu.equals("2")) {
+                    results.setQueue(result);
+                }
+
+                //조회
+                else if (menu.equals("2")) {
                     System.out.print("기준값보다 큰 값들을 조회합니다 기준값을 입력해주세요:");
                     int std = sc.nextInt();
                     results.getQueue(std).forEach((i) -> {
                         System.out.print(" " + i);
                     });
                     System.out.println();
-                } else if (menu.equals("3")) {
+
+                }
+
+                //삭제
+                else if (menu.equals("3")) {
                     System.out.println("제일 오래된 결과 값을 삭제 하시겠습니까? (y 입력 시 삭제)");
                     delete = sc.next();
                     if (delete.equals("y")) {
                         results.removeResult();
                     }
-                } else {
+                }
+
+                //종료
+                else {
                     if (menu.equals("4")) {
                         return;
                     }
-
                     System.out.println("유효한 값을 입력해주세요");
                     System.out.println("");
                 }
--- src/Lv3Calculator/com/example/calculator2/Calculator2.java
+++ src/Lv3Calculator/com/example/calculator2/Calculator2.java
@@ -10,51 +10,48 @@ public class Calculator2<T extends Number, U extends Number> {
         this.num2 = num2;
     }
 
+    // 정수,정수 인 경우와 나머지 경우 분류
     public Number calculate(OperatorType op) {
         if (this.num1 instanceof Integer && this.num2 instanceof Integer) {
             int n1 = this.num1.intValue();
             int n2 = this.num2.intValue();
             return this.intCal(n1, n2, op);
-        } else {
+        }
+        else {
             double n1 = this.num1.doubleValue();
             double n2 = this.num2.doubleValue();
             return this.doubleCal(n1, n2, op);
         }
     }
 
+    // 정수/정수 일때 계산
     public int intCal(int n1, int n2, OperatorType op) {
-        int var10000;
-        switch (op) {
-            case ADD -> var10000 = n1 + n2;
-            case SUBTRACT -> var10000 = n1 - n2;
-            case MULTIPLY -> var10000 = n1 * n2;
-            case DIVIDE -> var10000 = n1 / n2;
-            default -> throw new IncompatibleClassChangeError();
-        }
-
-        return var10000;
+        return switch (op) {
+            case ADD -> n1 + n2;
+            case SUBTRACT ->  n1 - n2;
+            case MULTIPLY ->  n1 * n2;
+            case DIVIDE ->  n1 / n2;
+        };
     }
 
+    // 나머지 경우 계산
     public double doubleCal(double n1, double n2, OperatorType op) {
-        double var10000;
-        switch (op) {
-            case ADD -> var10000 = n1 + n2;
-            case SUBTRACT -> var10000 = n1 - n2;
-            case MULTIPLY -> var10000 = n1 * n2;
-            case DIVIDE -> var10000 = n1 / n2;
-            default -> throw new IncompatibleClassChangeError();
-        }
-
-        return var10000;
+        return switch (op) {
+            case ADD -> n1 + n2;
+            case SUBTRACT ->  n1 - n2;
+            case MULTIPLY ->  n1 * n2;
+            case DIVIDE ->  n1 / n2;
+        };
     }
 
+    // 입력받은 연산자를 그에 맞게 형변환하는 메서드
     public static Number parseNumber(String Input) {
         try {
             return Integer.parseInt(Input);
-        } catch (NumberFormatException var4) {
+        } catch (NumberFormatException e1) {
             try {
                 return Double.parseDouble(Input);
-            } catch (NumberFormatException var3) {
+            } catch (NumberFormatException e2) {
                 throw new IllegalArgumentException("숫자를 입력하지 않았습니다.");
             }
         }
