484d80eaec69249e6c0f12c61b3fac21699a57c7
gkdl4239 <gkdl4239@gmail.com>
2024-11-18 14:14:37 +0900

    Refactor

--- src/Lv3Calculator/com/example/calculator2/App.java
+++ src/Lv3Calculator/com/example/calculator2/App.java
@@ -17,14 +17,15 @@ public class App {
         String menu;
         Number result = 0;
 
-        while(true) {
-            while(true) {
-                System.out.println("번호를 입력해주세요");
-                System.out.println("1.계산 2.조회 3.삭제 4.종료");
-                menu = sc.next();
+        while (true) {
 
-                //계산
-                if (menu.equals("1")) {
+            System.out.println("번호를 입력해주세요");
+            System.out.println("1.계산 2.조회 3.삭제 4.종료");
+            menu = sc.next();
+
+            //계산
+            switch (menu) {
+                case "1" -> {
                     System.out.print("첫 번째 숫자를 입력하세요:");
                     String input1 = sc.next();
                     Number num1 = Calculator2.parseNumber(input1);
@@ -33,7 +34,7 @@ public class App {
                     Number num2 = Calculator2.parseNumber(input2);
                     System.out.print("사칙 연산 기호를 입력하세요:");
                     String sign = sc.next();
-                    if(input2.equals("0")&&sign.equals("/")){
+                    if (input2.equals("0") && sign.equals("/")) {
                         System.out.print("분모는 0이 될수 없습니다");
                         continue;
                     }
@@ -41,44 +42,45 @@ public class App {
                     char operator = sign.charAt(0);
 
                     if (num1 instanceof Integer && num2 instanceof Integer) {
-                        cal1 = new Calculator2<>((Integer)num1,(Integer)num2);
+                        cal1 = new Calculator2<>((Integer) num1, (Integer) num2);
                         op = OperatorType.fromSymbol(operator);
                         result = cal1.calculate(op);
 
                     } else if (num1 instanceof Integer && num2 instanceof Double) {
-                        cal2 = new Calculator2<>((Integer)num1,(Double)num2);
+                        cal2 = new Calculator2<>((Integer) num1, (Double) num2);
                         op = OperatorType.fromSymbol(operator);
                         result = cal2.calculate(op);
 
                     } else if (num1 instanceof Double && num2 instanceof Integer) {
-                        cal3 = new Calculator2<>((Double)num1,(Integer)num2);
+                        cal3 = new Calculator2<>((Double) num1, (Integer) num2);
                         op = OperatorType.fromSymbol(operator);
                         result = cal3.calculate(op);
 
                     } else if (num1 instanceof Double && num2 instanceof Double) {
-                        cal4 = new Calculator2<>((Double)num1,(Double)num2);
+                        cal4 = new Calculator2<>((Double) num1, (Double) num2);
                         op = OperatorType.fromSymbol(operator);
                         result = cal4.calculate(op);
                     }
 
+                    result = results.decimal(result);
+                    results.setQueue(result);
+
                     System.out.println("결과 : " + result);
                     System.out.println("결과 값이 저장 되었습니다.");
-                    results.setQueue(result);
                 }
 
                 //조회
-                else if (menu.equals("2")) {
+                case "2" -> {
                     System.out.print("기준값보다 큰 값들을 조회합니다 기준값을 입력해주세요:");
                     int std = sc.nextInt();
                     results.getQueue(std).forEach((i) -> {
                         System.out.print(" " + i);
                     });
                     System.out.println();
-
                 }
 
                 //삭제
-                else if (menu.equals("3")) {
+                case "3" -> {
                     System.out.println("제일 오래된 결과 값을 삭제 하시겠습니까? (y 입력 시 삭제)");
                     delete = sc.next();
                     if (delete.equals("y")) {
@@ -87,7 +89,7 @@ public class App {
                 }
 
                 //종료
-                else {
+                default -> {
                     if (menu.equals("4")) {
                         return;
                     }
@@ -95,6 +97,7 @@ public class App {
                     System.out.println("");
                 }
             }
+
         }
     }
 }
--- src/Lv3Calculator/com/example/calculator2/Results.java
+++ src/Lv3Calculator/com/example/calculator2/Results.java
@@ -13,6 +13,15 @@ public class Results {
         this.queue.add(result);
     }
 
+    //부동 소수점 5자리까지 반올림하는 알고리즘
+    public Number decimal(Number result) {
+        Number rounded = 0;
+        if (result instanceof Double) {
+            rounded = Math.round(result.doubleValue() * 100000) / 100000.0;
+        }
+        return rounded;
+    }
+
     public void removeResult() {
         this.queue.poll();
     }
